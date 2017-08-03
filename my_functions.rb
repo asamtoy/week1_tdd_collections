74267dc275129d0d97921f4452dafdbf0a93e34f
David Ryan <davidryan1998@hotmail.com>
2017-08-03 15:05:35 +0100

    Dave's original files

---
+++ my_functions.rb
@@ -0,0 +1,35 @@
+def add_array_lengths (array1, array2)
+  array1.length + array2.length
+
+  # len1 = array1.length
+  # len2 = array2.length
+  #
+  # resultA = len1 + len2
+
+end
+
+def sum_array(array)
+  array.sum
+
+  # sum = 0
+  # for element in array
+  #   sum += element
+  # end
+  #
+  # return sum
+end
+
+
+def find_item(array,any_item)
+  array.find_index(any_item)!=nil    # Number if found. Nil if not found
+
+ # for selected_item in array do
+ #   return true if selected_item==any_item
+ # end
+ # return false
+
+end
+
+def get_first_key(hash)
+  hash.keys.first
+end
